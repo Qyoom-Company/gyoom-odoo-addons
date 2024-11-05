507215b8461bb9b58ccf18d26ac52ced0d0ff8b2
sameerz09 <sameerz09@hotmail.com>
2024-11-05 17:00:41 +0200

    compute hourly salary based on attendance

--- hr_hourly_payslip/models/__init__.py
+++ hr_hourly_payslip/models/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 #############################################################################
-#    A part of Open HRMS Project <https://www.openhrms.com>
 #
 #    Cybrosys Technologies Pvt. Ltd.
 #
-#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
 #    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
 #
 #    You can modify it under the terms of the GNU LESSER
@@ -21,18 +20,4 @@
 #
 #############################################################################
 from . import hr_contract
-from . import hr_contract_advantage_template
-from . import hr_contribution_register
-from . import hr_employee
-from . import hr_leave_type
-from . import hr_payroll_structure
 from . import hr_payslip
-from . import hr_payslip_input
-from . import hr_salary_rule
-from . import hr_payslip_line
-from . import hr_payslip_run
-from . import hr_payslip_worked_days
-from . import hr_rule_input
-from . import hr_salary_rule_category
-from . import res_config_settings
-from . import resource_mixin
